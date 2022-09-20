a86921f40abe35dbcb4db837d211870f22cf7e53
wendallR <wendallar125@gmail.com>
2022-09-20 18:16:20 -0400

    Update player sprite

--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -5,4 +5,5 @@ func _process(_delta: float) -> void:
 	var input := Vector2.ZERO
 	
 	input.x = Input.get_axis("move_left", "move_right")
+# warning-ignore:return_value_discarded
 	move_and_slide(input * 100, Vector2.UP)
