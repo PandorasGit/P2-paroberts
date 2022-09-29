544faab983e1a4f7e55b2b03ad37eed4ea5fc541
Roberts <waroberts@bsu.edu>
2022-09-29 16:43:57 -0400

    Add walk particles

--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -15,6 +15,8 @@ onready var _respawn_timer := find_node("RespawnTimer")
 onready var _sprite := find_node("AnimatedSprite")
 onready var _death_particles := find_node("DeathParticles")
 onready var _jump_timer := find_node("JumpTimer")
+onready var _walk_particles := find_node("WalkParticles")
+onready var _visuals := find_node("Visuals")
 
 
 func _ready() -> void:
@@ -50,12 +52,37 @@ func _calculate_input(input: Vector2) -> Vector2:
 
 func _caluculate_x_input() -> float:
 	var x_input = Input.get_axis("move_left", "move_right")
-	_sprite.play("idle" if x_input==0 else "walk")
-	if x_input < 0:
-		_sprite.scale.x = -1
-	elif x_input > 0:
-		_sprite.scale.x = 1
+	if not is_on_floor():
+		_animate_player_flying(x_input)
+	else:
+		_animate_player_walking(x_input)
 	return x_input
+
+
+func _animate_player_flying(x_input: float) -> void:
+	_sprite.play("idle")
+	_walk_particles.emitting = false
+	_set_visuals_direction(x_input)
+
+
+func _animate_player_walking(x_input: float) -> void:
+	_sprite.play("idle" if x_input==0 else "walk")
+	if x_input != 0:
+		_emit_walk_particles(x_input)
+	else:
+		_walk_particles.emitting = false
+
+
+func _emit_walk_particles(x_input: float) -> void:
+	_walk_particles.emitting = true
+	_set_visuals_direction(x_input)
+
+
+func _set_visuals_direction(x_input: float) -> void:
+	if x_input < 0:
+		_visuals.scale.x = -1
+	elif x_input > 0:
+		_visuals.scale.x = 1
 
 
 func _on_JumpTimer_timeout() -> void:
